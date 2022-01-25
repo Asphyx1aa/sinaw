c029f2f66845f0f64b4605271e5afd0b67aa3ff8
Timur <TiM76rus@yandex.ru>
2022-01-25 21:38:57 +0300

    Complete hero screen

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,25 @@ langPicker.addEventListener('click',() => {
     listLang.style.display = 'block';
     console.log('click')
 });
+
+const searchInput = document.querySelector('.hero-search__input');
+const learnSuggestion = document.querySelectorAll('.suggestion-list__button');
+
+learnSuggestion.forEach(e => {
+    e.addEventListener('click', () => {
+        searchInput.value = e.textContent;
+    })
+})
+
+const menuToggle = document.querySelector('.menu-button');
+let isMenuOpen = false;
+
+menuToggle.addEventListener('click', () => {
+    if(!isMenuOpen) {
+        menuToggle.classList.add('open');
+        isMenuOpen = true;
+    } else {
+        menuToggle.classList.remove('open');
+        isMenuOpen = false;
+    }
+})
